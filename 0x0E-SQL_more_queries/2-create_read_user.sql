c68e2a594e3715c01af3d59bac406dd068233132
Antony Wainaina <mainamasha254@gmail.com>
2024-06-13 03:19:25 +0300

    More

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- this script creates a database hbtn_0d_2 and user_0d_2
+-- the user password user_0d_2_pwd
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+USE hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
